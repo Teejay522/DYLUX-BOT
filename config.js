d77f2ea36ce8733d343ed96998bf32c67966e1ad
Teejay522 <128384934+Teejay522@users.noreply.github.com>
2023-09-01 02:56:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,15 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
-global.nomorbot = '254736958034' //Nomor Bot
+global.nomorbot = '254115085211' //Nomor Bot
 global.owner = [
-  ['254736958034', '🐝KHALID-TECH🐝', true],
-  ['923470027813'], '🌿xIRATOS CONTRIBUTORx🌿', true],
-  ['254768667910', '🐝STARBOY DONATOR🐝', true],
+  ['254115085211', '🐝KHALID-TECH🐝', true],
+  ['254115085211'], '🌿xIRATOS CONTRIBUTORx🌿', true],
+  ['254115085211', '🐝STARBOY DONATOR🐝', true],
 ] //Numeros
 
-global.mods = ['254736958034'] 
-global.prems = ['254736958034', '254716216161', '923470027813']
+global.mods = ['254115085211'] 
+global.prems = ['254115085211', '254716216161', '923470027813']
 global.APIs = { // API Prefix
   // name: 'https://website'
   xteam: 'https://api.xteam.xyz', 
